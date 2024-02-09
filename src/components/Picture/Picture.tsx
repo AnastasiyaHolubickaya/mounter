ac56b6410fc6aa8367253fe486749af06576bb06
AnastasiyaHolubickaya <kamenskaya77@ukr.net>
2024-02-09 21:21:52 +0200

    update path images Picture component

--- src/components/Picture/Picture.tsx
+++ src/components/Picture/Picture.tsx
@@ -5,7 +5,7 @@ import styles from './Picture.module.css';
 const Picture = () => {
   return (
     <div className={styles.wrapper}>
-      <img src="/public/monitors.png" alt="logo" className={styles.image} />
+      <img src="/monitors.png" alt="logo" className={styles.image} />
     </div>
   );
 };
