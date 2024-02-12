2dc9ba2de37cb52b2fe99b7e71273a159198500e
AnastasiyaHolubickaya <kamenskaya77@ukr.net>
2024-02-13 00:28:06 +0200

    remove unused import

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, memo, useEffect, useState } from 'react';
+import { SetStateAction, memo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
